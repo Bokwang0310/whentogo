b00647360d3d45a841749675f7f402d30d569d9a
Bokwang0310 <chdl2353@naver.com>
2020-06-24 22:13:38 +0900

    complete : basic of parsing

--- server/app.js
+++ server/app.js
@@ -1,7 +1,34 @@
-const app = require("express")();
+// const app = require("express")();
+const axios = require("axios");
+const parser = require("node-html-parser");
+const entityDecoder = require("html-entities").AllHtmlEntities;
+// const port = 8080;
 
-const port = 8080;
-
+/*
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
+});
+*/
+
+const url =
+  "https://search.naver.com/search.naver?&where=news&query=hello&sm=tab_pge&sort=0&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so:r,p:all,a:all&mynews=0&cluster_rank=37&start=1";
+
+const getHTML = async () => {
+  try {
+    return await axios.get(url);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getHTML().then((html) => {
+  const root = parser.parse(html.data, { lowerCaseTagName: true });
+  const rawText = root.querySelector("a._sp_each_title").rawText; // querySelectorAll로 배열로 싹 긁어오기
+
+  const entities = new entityDecoder();
+  console.log(entities.decode(rawText)); // + 배열 각 요소에 함수를 적용하고 다시 배열로 반환하는 함수 필요 : map인지 filter인지
+
+  // go 잔해들 싹 지우고 프론트에서도 json 포맷 새로 갈자
+  // 폴더구조도 client-server 말고 그냥 통합해서 노드모듈+패키지json+얀락 하나로 같이 쓰기
+  // -> 기존 모듈 싹 지우고 클라에 있는 패키지json에 노드 서버에 썼던 의존 다 추가 그리고 yarn으로 설치
 });
